43c4b00602b7e2ac1d033860604fdb2cc7f4119b
edeustace <ed.eustace@gmail.com>
2016-08-17 21:38:39 +0100

    fix the webpack<>react build

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # nvm use 6.0.0
+rm -rf dist
+mkdir dist
 babel --presets react src/corespring-multiple-choice-react.jsx --out-file dist/corespring-multiple-choice-react.js
 babel --presets react src/index.jsx --out-file dist/index.js
-webpack dist/index.js dist/bundle.js
+webpack 
 lessc src/index.less dist/index.css
