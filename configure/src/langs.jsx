f56ed3b91c45164029103bf28868bd378e6e8f66
Ed Eustace <ed.eustace@gmail.com>
2017-03-20 22:11:38 +0000

    add langs comp

---
+++ configure/src/langs.jsx
@@ -0,0 +1,10 @@
+import Checkbox from 'material-ui/Checkbox';
+import React from 'react';
+
+export default class Langs extends React.Component {
+
+  render() {
+    return <div>
+      <Checkbox label="enable multiple langs" /></div>;
+  }
+}
